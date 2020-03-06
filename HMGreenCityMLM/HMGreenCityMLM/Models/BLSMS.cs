095be2db531a7344123b261234d9178a9952e5ba
Afluex <contact.afluex@gmail.com>
2020-03-06 03:56:43 -0500

    changes

--- HMGreenCityMLM/HMGreenCityMLM/Models/BLSMS.cs
+++ HMGreenCityMLM/HMGreenCityMLM/Models/BLSMS.cs
@@ -31,7 +31,7 @@ namespace BusinessLayer
             //string strUrl = "http://smsw.co.in/API/WebSMS/Http/v1.0a/index.php?username=afluex&password=krishna@9919&sender=AFLUEX&to=8052949381&message=" + 
             //    User + "&password=" + password + "&senderid=" + senderid + "&route=2&number=" + Mobile_Number + "&message=" + Message;        // Create a request object  
 
-            string strUrl = "http://smsw.co.in/API/WebSMS/Http/v1.0a/index.php?username=afluex&password=krishna@9919&sender=DHARAW&to=" + Mobile_Number + "&message=" + Message + "& reqid = 1 & format ={ json}&route_id = 39 & callback =#&unique=0&sendondate='" + DateTime.Now.ToString() + " '";
+            string strUrl = "http://smsw.co.in/API/WebSMS/Http/v1.0a/index.php?username=afluex&password=krishna@9919&sender=AFLUEX&to=" + Mobile_Number + "&message=" + Message + "& reqid = 1 & format ={ json}&route_id = 39 & callback =#&unique=0&sendondate='" + DateTime.Now.ToString() + " '";
 
             WebRequest request = HttpWebRequest.Create(strUrl);
             // Get the response back  
