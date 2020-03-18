0bc5e28ff99ea6a35921eea980263b09012bc728
Shubham Srivastava <shubham.afluex@gmail.com>
2020-03-18 18:03:02 +0530

    Site,Sector, Block

--- HMGreenCityMLM/HMGreenCityMLM/Models/Master.cs
+++ HMGreenCityMLM/HMGreenCityMLM/Models/Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using System.Data.SqlClient;
+using System.Web.Mvc;
 
 namespace HMGreenCityMLM.Models
 {
@@ -26,6 +27,34 @@ namespace HMGreenCityMLM.Models
         public string NewsDate { get; set; }
         public string BV { get; set; }
         public List<Master> lstNews { get; set; }
+
+        public string SiteName { get; set; }
+        public string SiteTypeID { get; set; }
+        public string Rate { get; set; }
+        public string UnitID { get; set; }
+        public string Location { get; set; }
+        public string PLCName { get; set; }
+        public string PLCCharge { get; set; }
+        public string PLCID { get; set; }
+        public string DevelopmentCharge { get; set; }
+        public string SiteID { get; set; }
+        public string EncryptKey { get; set; }
+        public string UnitName { get; set; }
+        public string SiteTypeName { get; set; }
+        public DataTable dtPLCCharge { get; set; }
+
+        public List<Master> lstPLC { get; set; }
+        public List<Master> lstSite { get; set; }
+
+        public string SectorName { get; set; }
+        public string SectorID { get; set; }
+        public List<Master> lstSector { get; set; }
+        public List<SelectListItem> ddlSector { get; set; }
+
+
+        public string BlockID { get; set; }
+        public string BlockName { get; set; }
+        public List<Master> lstBlock1 { get; set; }
 
         #region ProductMaster
 
@@ -118,6 +147,201 @@ namespace HMGreenCityMLM.Models
                                   new SqlParameter("@DeletedBy", AddedBy),};
 
             DataSet ds = DBHelper.ExecuteQuery("DeleteNews", para);
+            return ds;
+        }
+
+        #endregion
+
+        #region Site
+
+        public DataSet GetUnitList()
+        {
+            DataSet ds = DBHelper.ExecuteQuery("GetUnitList");
+            return ds;
+        }
+
+        public DataSet GetSiteTypeList()
+        {
+
+            DataSet ds = DBHelper.ExecuteQuery("SiteTypeList");
+            return ds;
+        }
+
+        public DataSet GetSiteList()
+        {
+            SqlParameter[] para =
+            {
+                new SqlParameter("@PK_SiteID",SiteID)
+            };
+        
+            DataSet ds = DBHelper.ExecuteQuery("GetSiteName",para);
+            return ds;
+        }
+
+        public DataSet GetSitePlcChargeList()
+        {
+            SqlParameter[] para = { new SqlParameter("@SiteID", SiteID) };
+            DataSet ds = DBHelper.ExecuteQuery("SitePlcChargeList", para);
+            return ds;
+        }
+
+        public DataSet PLCList()
+        {
+            DataSet ds = DBHelper.ExecuteQuery("PLCList");
+            return ds;
+        }
+
+        public DataSet SaveSite()
+        {
+            SqlParameter[] para = { 
+                                      new SqlParameter("@SiteName", SiteName),
+                                      new SqlParameter("@Location", Location),
+                                      new SqlParameter("@AddedBy", AddedBy)
+
+            };
+            DataSet ds = DBHelper.ExecuteQuery("SiteMaster", para);
+            return ds;
+        }
+
+        public DataSet UpdateSite()
+        {
+            SqlParameter[] para = {   
+                                      new SqlParameter("@SiteName", SiteName),
+                                      new SqlParameter("@Location", Location),
+                                      new SqlParameter("@UpdatedBy", UpdatedBy),
+
+                                      new SqlParameter("@SiteID", SiteID)
+
+            };
+            DataSet ds = DBHelper.ExecuteQuery("UpdateSite", para);
+            return ds;
+        }
+
+        public DataSet DeleteSite()
+        {
+            SqlParameter[] para = { new SqlParameter("@SiteID", SiteID),
+                                  new SqlParameter("@DeletedBy", UpdatedBy) };
+            DataSet ds = DBHelper.ExecuteQuery("DeleteSite", para);
+            return ds;
+        }
+
+
+        public DataSet GetSectorList()
+        {
+            SqlParameter[] para = { new SqlParameter("@SiteID", SiteID) };
+            DataSet ds = DBHelper.ExecuteQuery("GetSectorList", para);
+            return ds;
+        }
+
+
+        public DataSet GetPLCChargeList()
+        {
+            SqlParameter[] para = { new SqlParameter("@FK_SiteID", SiteID) };
+            DataSet ds = DBHelper.ExecuteQuery("GetPlotPLCCharge", para);
+            return ds;
+
+        }
+
+        #endregion
+
+          #region  Sector
+
+        public DataSet SaveSector()
+        {
+            SqlParameter[] para =
+                            {
+
+                                new SqlParameter("@FK_SiteID",SiteID),
+                                new SqlParameter("@SectorName",SectorName),
+                                new SqlParameter("@AddedBy",AddedBy)
+
+                            };
+            DataSet ds = DBHelper.ExecuteQuery("SectorMaster", para);
+            return ds;
+        }
+
+
+        public DataSet UpdateSector()
+        {
+            SqlParameter[] para =
+                            {
+                                new SqlParameter("@PK_SectorID",SectorID),
+                                new SqlParameter("@FK_SiteID",SiteID),
+                                new SqlParameter("@SectorName",SectorName),
+                                new SqlParameter("@UpdatedBy",UpdatedBy)
+
+                            };
+            DataSet ds = DBHelper.ExecuteQuery("UpdateSector", para);
+            return ds;
+        }
+
+        public DataSet GetSector()
+        {
+            SqlParameter[] para =
+             {
+                                new SqlParameter("@PK_SectorID",SectorID)
+
+                            };
+            DataSet ds = DBHelper.ExecuteQuery("SelectSector", para);
+            return ds;
+        }
+       
+
+        public DataSet DeleteSector()
+        {
+            SqlParameter[] para = { new SqlParameter("@PK_SectorID", SectorID),
+                                  new SqlParameter("@DeletedBy", UpdatedBy) };
+            DataSet ds = DBHelper.ExecuteQuery("DeleteSector", para);
+            return ds;
+        }
+
+        #endregion
+
+        #region BlockMaster
+
+        public DataSet DeleteBlock()
+        {
+            SqlParameter[] para = { new SqlParameter("@PK_BlockID", BlockID),
+                                  new SqlParameter("@DeletedBy", UpdatedBy) };
+            DataSet ds = DBHelper.ExecuteQuery("DeleteBlock", para);
+            return ds;
+        }
+
+
+        public DataSet SaveBlock()
+        {
+            SqlParameter[] para = {
+                                      new SqlParameter("@FK_SiteID", SiteID),
+                                      new SqlParameter("@BlockName", BlockName),
+                                      new SqlParameter("@FK_SectorID", SectorID),
+                                      new SqlParameter("@AddedBy", AddedBy)
+
+                                  };
+            DataSet ds = DBHelper.ExecuteQuery("BlockMaster", para);
+            return ds;
+        }
+
+        public DataSet UpdateBlock()
+        {
+            SqlParameter[] para = {
+                                      new SqlParameter("@PK_BlockID", BlockID),
+                                      new SqlParameter("@FK_SiteID ", SiteID ),
+                                      new SqlParameter("@FK_SectorID", SectorID),
+                                        new SqlParameter("@BlockName", BlockName),
+                                      new SqlParameter("@UpdatedBy", UpdatedBy)
+
+                                  };
+            DataSet ds = DBHelper.ExecuteQuery("UpdateBlockMaster", para);
+            return ds;
+        }
+
+        public DataSet GetBlockList()
+        {
+            SqlParameter[] para ={ new SqlParameter("@SiteID",SiteID),
+                                     new SqlParameter("@SectorID",SectorID),
+                                     new SqlParameter("@BlockID",BlockID),
+                                 };
+            DataSet ds = DBHelper.ExecuteQuery("GetBlockList", para);
             return ds;
         }
 
