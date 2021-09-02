7d4a78bc1bd97510d1d1f085aa1bfa31d93447e4
afluex-sudhakar <developer2.afluex@gmail.com>
2021-09-02 12:17:48 +0530

    AddAddharNumber

--- HMGreenCityMLM/HMGreenCityMLM/Models/DBHelper.cs
+++ HMGreenCityMLM/HMGreenCityMLM/Models/DBHelper.cs
@@ -17,9 +17,9 @@ namespace HMGreenCityMLM.Models
             try
             {
                 //connectionString = "Data Source=103.48.51.111,1232;Initial Catalog=hmgreendb;User Id=hmgreenuser; Password=W1ttl1&9; Integrated Security=false;";
-                //connectionString = "Data Source=164.52.211.92,1232;Initial Catalog=hmgreendb;User Id=hmgreenusernew; Password=W1ttl1&9; Integrated Security=false;";
+                connectionString = "Data Source=164.52.211.92,1232;Initial Catalog=hmgreendb;User Id=hmgreenusernew; Password=W1ttl1&9; Integrated Security=false;";
 
-                connectionString = "Data Source=164.52.211.92,1232;Initial Catalog=hmgreendbtest;User Id=hmgreenusernew; Password=W1ttl1&9; Integrated Security=false;";
+                //connectionString = "Data Source=164.52.211.92,1232;Initial Catalog=hmgreendbtest;User Id=hmgreenusernew; Password=W1ttl1&9; Integrated Security=false;";
                 //connectionString = "Data Source=DESKTOP-KPDI8EN\\AFLUEX;Initial Catalog=HMGreenCityMLM;User Id=sa;Password=123;Integrated Security=false;";
             }
             catch (Exception)
